bced1e11d89fcafaf3d837aadfed7397b911285a
Chise Ciprian <chise.ciprian05@gmail.com>
2019-07-30 10:12:15 +0300

    tema curs 16

--- src/main/java/ro/itschool/mvnbase/homeWorkCurs16/BirthYearCalculatorEx1/Main.java
+++ src/main/java/ro/itschool/mvnbase/homeWorkCurs16/BirthYearCalculatorEx1/Main.java
@@ -1,8 +1,22 @@
 package ro.itschool.mvnbase.homeWorkCurs16.BirthYearCalculatorEx1;
 
+import ro.itschool.mvnbase.homeWorkCurs16.BirthYearCalculatorEx2.BadInputException;
+import ro.itschool.mvnbase.homeWorkCurs16.BirthYearCalculatorEx2.InputEvaluator;
+
 public class Main {
     public static void main(String[] args) {
-        BirthYearCalculator birthYearCalculator=new BirthYearCalculator(20);
-        System.out.println(birthYearCalculator.calculateBirth());
+        InputEvaluator inputEvaluator=new InputEvaluator();
+
+        try {
+            int age=20;
+            inputEvaluator.evaluate(age);
+            BirthYearCalculator birthYearCalculator=new BirthYearCalculator(age);
+            System.out.println(birthYearCalculator.calculateBirth());
+        }catch (BadInputException bie){
+            System.out.println(bie);
+        }
+
+
+
     }
 }
