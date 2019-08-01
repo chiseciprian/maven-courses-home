0072e1720017ee2918a84bb4421fd6a2b3c8f77f
Chise Ciprian <chise.ciprian05@gmail.com>
2019-08-01 17:57:20 +0300

    tema curs 17

--- src/main/java/ro/itschool/mvnbase/homeWorkCurs17/Ex4/ZeroInNumber.java
+++ src/main/java/ro/itschool/mvnbase/homeWorkCurs17/Ex4/ZeroInNumber.java
@@ -2,13 +2,15 @@ package ro.itschool.mvnbase.homeWorkCurs17.Ex4;
 
 public class ZeroInNumber {
     public int zeroInNumber(int num) {
-        if (num==0)
+        if (num == 0) {
             return 1;
-        if(num % 10 == 0)
+        }
+        if (num % 10 == 0) {
             return 1 + zeroInNumber(num / 10);
-        else if (num / 10 == 0)
+        } else if (num / 10 == 0) {
             return 0;
-        else
+        } else {
             return zeroInNumber(num / 10);
+        }
     }
 }
